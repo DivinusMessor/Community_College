7359af9c79aab341bb34fea7882dc6487a77f3d3
Yukio <ysrivera@jeff.cis.cabrillo.edu>
2021-07-20 21:16:29 -0700

    Finished the Lispify code

--- FHc++/Elisa.cpp
+++ FHc++/Elisa.cpp
@@ -7,6 +7,8 @@
 // 
 #include <iostream> 
 #include <sstream> 
+#include <string>
+#include <algorithm>
  
 using namespace std; 
  
@@ -16,13 +18,29 @@ using namespace std;
 // Note that the string is passed in by reference. So the caller may not 
 // rely on the result being returned. 
 // TODO - Your code for rotate_vowels goes here
-int rotate_vowels(string str){
-    cout << size(str);
+int rotate_vowels(string strg){
+    // cout << strg.length(); //This returns the length of the str
+    // string vowels[strg.length()] = {};
+    // for (int i=0; i<strg.length(); i++){
+    //     temp = strg[i];
+    //     if temp in 
+    // }
     return 0;
 } 
  
 // Return a string in which all occurrences of s have been replaced by th 
 // TODO - Your code for lispify goes here 
+string lispify(string strg){
+    for (int i=0; i<strg.length(); i++){
+        if (strg[i] == 's'){
+            strg.replace(i, 1, "th");;
+        } 
+        else if (strg[i] == 'S'){
+            strg.replace(i, 1, "Th");
+        }
+    }
+    return strg;
+}
  
 // Enter the user-interaction loop as described earlier 
 void enter() { 
@@ -31,5 +49,6 @@ void enter() {
 }
 
 int main(){
-    cout << rotate_vowels("hat")
+    //cout << rotate_vowels("hats");
+    cout << lispify("sixSix");
 }
