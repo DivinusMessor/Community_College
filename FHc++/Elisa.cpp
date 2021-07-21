818b9cdf22468df80f2f930e809e90f3c0193bce
Yukio <ysrivera@jeff.cis.cabrillo.edu>
2021-07-20 19:03:40 -0700

    Added some stuff to Elisa

---
+++ FHc++/Elisa.cpp
@@ -0,0 +1,35 @@
+//  Student ID:20288575
+//  Yukio Rivera
+//
+//  Eliza.cpp 
+//  2a.Lab-05-Eliza 
+// 
+// 
+#include <iostream> 
+#include <sstream> 
+ 
+using namespace std; 
+ 
+// Return a new string in which the letters (lowercase) a, e, i, o, and u 
+// have been replaced by the next vowel in the sequence aeiou. Replace u by a. 
+// 
+// Note that the string is passed in by reference. So the caller may not 
+// rely on the result being returned. 
+// TODO - Your code for rotate_vowels goes here
+int rotate_vowels(string str){
+    cout << size(str);
+    return 0;
+} 
+ 
+// Return a string in which all occurrences of s have been replaced by th 
+// TODO - Your code for lispify goes here 
+ 
+// Enter the user-interaction loop as described earlier 
+void enter() { 
+    // TODO - Your code here 
+    ;
+}
+
+int main(){
+    cout << rotate_vowels("hat")
+}
