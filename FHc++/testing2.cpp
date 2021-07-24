ffd8ed59b9bc509ac3d64ccd7c76e839704466f7
Yukio <ysrivera@jeff.cis.cabrillo.edu>
2021-07-23 18:09:47 -0700

    finishing enter()

--- FHc++/testing2.cpp
+++ FHc++/testing2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 using namespace std;
 // Testing on how to do the vowels
 
@@ -10,7 +11,23 @@ void enter(){
         if (answer.empty()){
             enter();
         }
+        else if (answer.find('!') != std::string::npos){
+            cout << "OMG! You don't say!!  " << answer << "!!!!!" << endl;
+            enter();
+        }
+        else if (answer == "what?" | answer == "why?"){
+            cout << "I'm sorry, I don't like questions that contain what or why." << endl;
+            enter();
+        }   
+        else if (answer.find('s') != std::string::npos){
+            cout << "Interethting. When did you thtop thtopping your thibilanth?" << endl;
+            lispify(answer);
+            enter();
+        }
         cout << "    " << answer << endl;
+        cout << endl;
+        cout << endl;
+        
     }
 }
 
