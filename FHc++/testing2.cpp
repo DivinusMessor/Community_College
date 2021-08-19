6732cffc53d68ca8caea0c4eb35a2741b9ea0d5f
DivinusMessor <DivinusMessor@users.noreply.github.com>
2021-08-18 18:57:19 -0700

    example

--- FHc++/testing2.cpp
+++ FHc++/testing2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 // Testing on how to do the vowels
@@ -8,12 +9,20 @@ string make_a_name(int len){
     string n_name = "";
     string temp = "hi";
     string vowels = "aeiou";
+    //21 constants
     string constants = "bcdfghjklmnpqrstvwxyz";
     int counter = 0;
     // Going to add a counter to swtich between the vowels and consts
-    if (rand() % 2 != 0){
+    srand(time(NULL));
+    int rand_num = rand() % 10 + 1;
+    int rand_con = rand() % 21;
+    cout << rand_num;
+    cout << rand_con;
+    if (rand_num % 2 == 0){
         for (int i = 0; i < constants.length(); i++){
-            cout << ( rand() % (10 + 1 ) ) << endl;
+            n_name[counter] = constants[rand()];
+            counter ++;
+            cout << n_name << endl;
         }
     }
     return temp;
